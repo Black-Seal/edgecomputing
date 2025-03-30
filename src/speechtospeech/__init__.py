0b4dd211341e197ff7bc020dbc7e3d8d18c82ea9
Justin <2302675@sit.singaporetech.edu.sg>
2025-03-30 20:15:03 +0800

    Updated requirements

--- src/speechtospeech/__init__.py
+++ src/speechtospeech/__init__.py
@@ -1,5 +1,3 @@
-from .can_to_eng import Can_to_eng
-from .eng_to_can import Eng_to_can
 from .multilingual_translator import MultilingualTranslator
 
-__all__ = ['Can_to_eng', 'Eng_to_can', 'MultilingualTranslator']
+__all__ = ['MultilingualTranslator']
