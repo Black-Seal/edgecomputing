1bc11e43e5d67e00f75cce203bbd942dbba75742
Justin <tanyonganjustin@gmail.com>
2025-04-01 08:48:10 +0800

    removed unecessary files from branch after final testing

--- src/speechtospeech/__init__.py
+++ src/speechtospeech/__init__.py
@@ -1,6 +1,3 @@
-# from .can_to_eng import Can_to_eng
-# from .eng_to_can import Eng_to_can
-# from .multilingual_translator import MultilingualTranslator
 from .simple_translator import SimpleTranslator
 
 __all__ = ['SimpleTranslator']
